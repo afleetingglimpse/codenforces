e01ae1f58ddc3c61b5aadc950829b6cb6eedff6d
Egor <you@example.com>
2023-04-17 21:51:42 +0300

    scada methods impl

--- src/main/java/ru/codenforces/demo/controller/SensorController.java
+++ src/main/java/ru/codenforces/demo/controller/SensorController.java
@@ -1,7 +1,10 @@
 package ru.codenforces.demo.controller;
 
-@RestController
-@RequestMapping(path = "/api")
-public class EventController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping(path = "/signals")
+public class SensorController {
 
 }
---
+++ src/main/java/ru/codenforces/demo/model/CustomError.java
@@ -0,0 +1,28 @@
+package ru.codenforces.demo.model;
+
+public class CustomError {
+    private int errorCode;
+    private String msg;
+
+    public CustomError(int errorCode, String msg) {
+        this.errorCode = errorCode;
+        this.msg = msg;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+}
