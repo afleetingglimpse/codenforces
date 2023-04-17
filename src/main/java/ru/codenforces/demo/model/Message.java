e429ef61c4303bc8d07b3d068e63351131fd5613
Egor <you@example.com>
2023-04-17 20:53:25 +0300

    scada stub

--- src/main/java/ru/codenforces/demo/model/Message.java
+++ src/main/java/ru/codenforces/demo/model/Message.java
@@ -1,2 +1,13 @@
-package ru.codenforces.demo.model;public class Message {
+package ru.codenforces.demo.model;
+
+public class Message {
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
