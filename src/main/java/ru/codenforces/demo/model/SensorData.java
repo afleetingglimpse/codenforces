5a664ff69d7fcd9fdf1f3e655b8c298b55d83b59
Kirill <kirill.ustimencko@gmail.com>
2023-04-18 13:52:30 +0300

    added model.SensorData to represent data sent by Sensor

---
+++ src/main/java/ru/codenforces/demo/model/SensorData.java
@@ -0,0 +1,13 @@
+package ru.codenforces.demo.model;
+public class SensorData {
+    int data;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+}
--- src/main/java/ru/codenforces/demo/sensor/Sensor.java
+++ src/main/java/ru/codenforces/demo/sensor/Sensor.java
@@ -2,31 +2,27 @@ package ru.codenforces.demo.sensor;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
-
-@Component
-@PropertySource("application.properties")
 public class Sensor {
 
     private static final long DELIVERY_INTERVAL_MILLISEC = 500;
     private static final int SIGNAL_RANGE = 20;
 
-    @Value("${sensor.uri}")
-    private static String uri;
-    public static void main() throws InterruptedException, JsonProcessingException {
+    // тут кринж
+    private static final String uri = "http://localhost:8080/signals";
+
+    public static void main(String[] args) throws InterruptedException, JsonProcessingException {
         while (true) {
-            TimeUnit.SECONDS.sleep(DELIVERY_INTERVAL_MILLISEC);
+
+            TimeUnit.SECONDS.sleep(DELIVERY_INTERVAL_MILLISEC/1000);
 
             Random random = new Random();
             int data = random.nextInt(SIGNAL_RANGE);
@@ -40,13 +36,14 @@ public class Sensor {
                     .uri(URI.create(uri))
                     .build();
             try {
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()) ;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                // logger here
+                System.out.println("Request sent");
+            }
+            catch (IOException e) {
+                System.out.println(e.getMessage());
+                // logger here
             }
         }
     }
-
-
-
 }
