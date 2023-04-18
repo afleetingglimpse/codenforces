43bf6eb7128646d923720380aa5f96782cb2d985
Egor <you@example.com>
2023-04-18 21:10:14 +0300

    formatting

--- src/main/java/ru/codenforces/demo/sensor/Sensor.java
+++ src/main/java/ru/codenforces/demo/sensor/Sensor.java
@@ -3,6 +3,7 @@ package ru.codenforces.demo.sensor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.codenforces.demo.Utils;
+import ru.codenforces.demo.service.DeviceMsgSender;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -17,14 +18,15 @@ import java.util.Arrays;
 import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
 import static java.net.URI.create;
 public class Sensor {
-
     private static final long DELIVERY_INTERVAL_MILLISEC = 500;
     private static final int SIGNAL_RANGE = 20;
-
     // тут кринж
-//    private static final String URI = "http://localhost:8080/signals";
+    //"http://localhost:8080/signals";
+    public static final Logger LOGGER = Logger.getLogger(DeviceMsgSender.class.getName());
     private static final String URI = Utils.getProperty("sensor.uri");
 
     public static void main(String[] args) throws InterruptedException, JsonProcessingException {
@@ -46,12 +48,10 @@ public class Sensor {
                     .build();
             try {
                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                // logger here
-                System.out.println("Request sent");
+                LOGGER.warning("Request sent");
             }
             catch (IOException e) {
-                System.out.println(Arrays.asList(e.getStackTrace()));//
-                // logger here
+                LOGGER.warning(Arrays.toString(e.getStackTrace()));
             }
         }
     }
