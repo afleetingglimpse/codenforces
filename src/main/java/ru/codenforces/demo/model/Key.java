eff6148d0b5cc7bb0e379a20695771ff7ff2d4d7
Kirill <kirill.ustimencko@gmail.com>
2023-04-18 20:45:00 +0300

    added DeviceController, Key

---
+++ src/main/java/ru/codenforces/demo/model/Key.java
@@ -0,0 +1,13 @@
+package ru.codenforces.demo.model;
+
+public class Key {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
