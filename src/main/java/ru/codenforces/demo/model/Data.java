b6fd751e2d463541e736949e40076f9b3594a02e
Kirill <kirill.ustimencko@gmail.com>
2023-04-19 21:40:17 +0300

    added constructors to model classes

--- src/main/java/ru/codenforces/demo/model/Data.java
+++ src/main/java/ru/codenforces/demo/model/Data.java
@@ -5,6 +5,14 @@ public class Data {
     private boolean status;
     private int value;
 
+    public Data() {}
+
+    public Data(String operation, boolean status, int value) {
+        this.operation = operation;
+        this.status = status;
+        this.value = value;
+    }
+
     public String getOperation() {
         return operation;
     }
--- src/main/java/ru/codenforces/demo/model/Key.java
+++ src/main/java/ru/codenforces/demo/model/Key.java
@@ -1,13 +1,10 @@
 package ru.codenforces.demo.model;
 
 public class Key {
+
+    public Key(String name) {this.name = name;}
+
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
 }
