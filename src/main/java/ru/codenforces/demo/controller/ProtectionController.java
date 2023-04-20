7709a3e5721d9fc017aca71df7eb43110a0e3eb1
Kirill <kirill.ustimencko@gmail.com>
2023-04-20 23:47:24 +0300

    minor changes

--- src/main/java/ru/codenforces/demo/controller/ProtectionController.java
+++ src/main/java/ru/codenforces/demo/controller/ProtectionController.java
@@ -1,10 +1,18 @@
 package ru.codenforces.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.codenforces.demo.model.Data;
 import ru.codenforces.demo.service.DeviceMsgSender;
+import ru.codenforces.demo.service.ProtectionService;
 
+import java.net.http.HttpResponse;
 import java.util.logging.Logger;
 
 @RestController
@@ -12,7 +20,13 @@ import java.util.logging.Logger;
 public class ProtectionController {
     public static final Logger LOGGER = Logger.getLogger(DeviceMsgSender.class.getName());
 
+    @Autowired
+    private ProtectionService protectionService;
+
     @PostMapping
-    public
+    public ResponseEntity<?> alarm(@RequestBody Data data) {
+        protectionService.react(data);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
