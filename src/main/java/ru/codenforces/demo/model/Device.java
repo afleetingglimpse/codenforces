493bfbd7ee4fe7fe01ffa0ef4be5abb6bfdfc60a
Egor <you@example.com>
2023-04-21 23:03:21 +0300

    added device and event functions

---
+++ src/main/java/ru/codenforces/demo/model/Device.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023.
+ * Egor Adonev, Artyom Kornilov
+ */
+
+package ru.codenforces.demo.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.codenforces.demo.model.Data;
+import ru.codenforces.demo.service.DeviceMsgSender;
+import ru.codenforces.demo.service.ProtectionService;
+import ru.codenforces.demo.service.SettingsManager;
+
+import java.security.SecureRandom;
+import java.lang.Object;
+
+public class Device {
+//	private static String KEY = "12345";
+	public Event event = new Event();
+	@Autowired
+	private DeviceMsgSender deviceMsgSender;
+	@Autowired
+	private SettingsManager settingsManager;
+	public void cron (int t) throws JsonProcessingException, InterruptedException, IllegalAccessException {
+		Settings settings = settingsManager.loadSettings();
+		settingsManager.settingsSanityCheck(settings);
+		while (!event.isSet()){
+			Thread thread = Thread.currentThread();
+			thread.sleep(t);
+			SecureRandom random = new SecureRandom();
+			int key = random.nextInt();
+			Data secondDat = new Data("send_diagnostic");
+			//"Checked with status: "
+			secondDat.setStatus(true);
+			deviceMsgSender.sendDigitalData(secondDat);
+			Data firstDat = new Data("send_key");
+			firstDat.setValue(key);
+			deviceMsgSender.sendDigitalData(firstDat);
+		}
+	}
+	
+}
