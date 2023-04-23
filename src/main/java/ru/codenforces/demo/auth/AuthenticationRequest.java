19b9ca86967f013db7ad9e4ea1901ea87e3e965b
Egor <you@example.com>
2023-04-23 22:01:19 +0300

    final fix

--- src/main/java/ru/codenforces/demo/auth/AuthenticationRequest.java
+++ src/main/java/ru/codenforces/demo/auth/AuthenticationRequest.java
@@ -1,30 +1,30 @@
-package ru.codenforces.demo.auth;
-
-public class AuthenticationRequest {
-    public AuthenticationRequest() {
-    }
-
-    public AuthenticationRequest(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    private String name;
-    private String password;
-}
+//package ru.codenforces.demo.auth;
+//
+//public class AuthenticationRequest {
+//    public AuthenticationRequest() {
+//    }
+//
+//    public AuthenticationRequest(String name, String password) {
+//        this.name = name;
+//        this.password = password;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    private String name;
+//    private String password;
+//}
--- src/main/java/ru/codenforces/demo/auth/RegisterRequest.java
+++ src/main/java/ru/codenforces/demo/auth/RegisterRequest.java
@@ -1,31 +1,31 @@
-package ru.codenforces.demo.auth;
-
-public class RegisterRequest {
-    public RegisterRequest() {
-    }
-
-    public RegisterRequest(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    private String name;
-    private String password;
-
-}
+//package ru.codenforces.demo.auth;
+//
+//public class RegisterRequest {
+//    public RegisterRequest() {
+//    }
+//
+//    public RegisterRequest(String name, String password) {
+//        this.name = name;
+//        this.password = password;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    private String name;
+//    private String password;
+//
+//}
